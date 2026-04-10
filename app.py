fc663bac74b48b5219ae9c1b3d1d1236de56026b
amine-za <aminezaghloul2003@gmail.com>
2026-04-10 02:43:20 +0200

    feat: display formatted restaurant list in CLI
    Show first 10 restaurants with name, cuisines, rating, and address in a readable console format.

--- app.py
+++ app.py
@@ -1,21 +1,53 @@
-import requests, cloudscraper
+import requests
 
-def get_data() -> dict:
+
+ORANGE = '\033[1m\033[38;5;214m'
+WARNING = '\033[93m'
+FAIL = '\033[91m'
+BOLD = '\033[1m'
+END = '\033[0m'
+
+
+def get_data():
     url = "https://uk.api.just-eat.io/discovery/uk/restaurants/enriched/bypostcode/EC4M7RF"
-
-    scraper = cloudscraper.create_scraper() # use cloudscraper to bypass Cloudflare's anti-bot protection in the jet api
-    response = scraper.get(url)
+    headers = {
+        "User-Agent": "Mozilla",
+        "Accept": "application/json"
+    }
+    response = requests.get(url, headers=headers)
 
     if response.status_code == 200:
-        return response.text
+        return (response.json())
     else:
         raise Exception(f"Failed to fetch data: {response.status_code}")
 
+
 def main():
     try:
-        data = get_data()
-        print(data)
+        data = get_data() # Fetch the restaurant json data from the API
+
+        # Loop through the first 10 restaurants and print their details
+        for index, restaurant in enumerate(data["restaurants"][:10]):
+            cuisines = ", ".join([cuisine['name'] for cuisine in restaurant['cuisines']])
+            rating_num = int(restaurant['rating']['starRating'])
+            address = (
+                restaurant['address']['firstLine']  + ', ' +
+                restaurant['address']['city']       + ', ' +
+                restaurant['address']['postalCode']
+            )
+            
+            print(f"{ORANGE}{'='*55}{END}")
+            print(f"{ORANGE} RESTAURANT #{index+1} {END}".center(55))
+            print(f"{ORANGE}{'='*55}{END}")
+
+            print(f"{BOLD}Name       :{END} {restaurant['name']}")
+            print(f"{BOLD}Cuisines   :{END} {cuisines} ")
+            print(f"{BOLD}Rating     :{END} {rating_num} {'★ ' * rating_num}")
+            print(f"{BOLD}Address    :{END} {address}", end="\n\n\n")
+
+    # Handle any exceptions that occur during the process
     except Exception as e:
         print(e)
 
 main()
+        
